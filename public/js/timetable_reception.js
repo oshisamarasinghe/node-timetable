dc31df7f8c6ac36557dd930aecc42517a08b02de
oshadi <oshadhi.15@cse.mrt.ac.lk>
2017-07-12 00:06:49 +0530

    add timetable|function

--- public/js/timetable_reception.js
+++ public/js/timetable_reception.js
@@ -1,5 +1,6 @@
 /*
 oshadhi-2017/07/10
+**oshadhi-2017/07/11
 */
 
 var socket=io();
@@ -16,57 +17,30 @@ socket.on('disconnect',function(){
 });
 
 
-
-$("#ttype").on('change',function(e){
-  var type=$(this).val(); ;
-  console.log('type' ,type);
-});
-
-var addbtn =$('#add');
-addbtn.on('click', function(e){
+//new timetable  add function
+function addTable() {
   var id=$('#inputID').val() ;
+  var type=$('#ttype').val();
 
-  if(id==='' && id.trim()===''){
-    return alert('id not entered');
+if(id==='' || id.trim()===''){
+    console.log('id not entered');
   }
-
-});
-
-
-/**$("#inputID").on('click',function(e){
-  e.preventDefault();
-});
-
-
-
-
-/*function add_timetable(){
-  var type=document.getElementById('ttype').value;
-  var id=document.getElementById('inputID').value  ;
-
-  var c11= document.getElementById("ac012").innerHTML;
-  var c12= document.getElementById("ac013").innerHTML;
-  var c13= document.getElementById("ac014").innerHTML;
-  var c14= document.getElementById("ac015").innerHTML;
-  var c15= document.getElementById("ac016").innerHTML;
-  var c16= document.getElementById("ac017").innerHTML;
+ else{
+    socket.emit('addNewTable',{
+      tbltype: type,
+      tblid : id,
+      row1  :  [$('#ac012').text(),$('#ac013').text(),$('#ac014').text(),$('#ac015').text(),$('#ac016').text(),$('#ac017').text()] ,
+      row2  :  [$('#ac022').text(),$('#ac023').text(),$('#ac024').text(),$('#ac025').text(),$('#ac026').text(),$('#ac027').text()],
+      row3  :  [$('#ac032').text(),$('#ac033').text(),$('#ac034').text(),$('#ac035').text(),$('#ac036').text(),$('#ac037').text()],
+      row4  :  [$('#ac042').text(),$('#ac043').text(),$('#ac044').text(),$('#ac045').text(),$('#ac046').text(),$('#ac047').text()],
+      row6  :  [$('#ac062').text(),$('#ac063').text(),$('#ac064').text(),$('#ac065').text(),$('#ac066').text(),$('#ac067').text()],
+      row7  :  [$('#ac072').text(),$('#ac073').text(),$('#ac074').text(),$('#ac075').text(),$('#ac076').text(),$('#ac077').text()],
+      row8  :  [$('#ac082').text(),$('#ac083').text(),$('#ac084').text(),$('#ac085').text(),$('#ac086').text(),$('#ac087').text()],
+      row9  :  [$('#ac092').text(),$('#ac093').text(),$('#ac094').text(),$('#ac095').text(),$('#ac096').text(),$('#ac097').text()],
+      row10  :  [$('#ac102').text(),$('#ac103').text(),$('#ac104').text(),$('#ac105').text(),$('#ac106').text(),$('#ac107').text()],
+      row11  :  [$('#ac112').text(),$('#ac113').text(),$('#ac114').text(),$('#ac115').text(),$('#ac116').text(),$('#ac117').text()]
+    });
 
 
-}*/
-
-
-/* $('#add').on('onClick',function(e){
-  e.preventDefault();
-});
-
-    /*var ntimetable = {
-        timetableType:$('[id=ttype]').val(),
-        timetableId:$('[id=inputID]').val(),
-        r1c1:$('[id=ac011]').innerHTML,
-        r2c2:$('[id=ac012]').innerHTML
-
-    };
-
-    socket.emit('createNewTimeTable', ntimetable, function (message) {
-        alert(message);
-    });*/
+}
+}
--- server/server.js
+++ server/server.js
@@ -1,7 +1,154 @@
+/*
+* oshadhi-2017/07/11
+*/
 const path=require('path');
 const express=require('express');
 const http=require('http');
 const socketIO=require('socket.io');
+const MongoClient =require('mongodb').MongoClient;
+
+//local imports
+var {mongoose} = require('./db/mongoose.js');
+
+var SemTimeTable= mongoose.model('SemTimeTable',{
+_id:{
+  type: String,
+  min:5
+},
+tbltype:{
+  type:String,
+  default:'sem'
+},
+row1:{
+   type:[String]
+},
+row2:{
+  type:[String]
+},
+row3:{
+  type:[String]
+},
+row4:{
+  type:[String]
+},
+row6:{
+   type:[String]
+},
+row7:{
+ type:[String]
+},
+row8:{
+   type:[String]
+},
+row9:{
+   type:[String]
+},
+row10:{
+ type:[String]
+},
+row11:{
+   type:[String]
+}
+});
+//var {TimeTable}= require('./models/timetableModel.js');
+
+//lec timetable model
+var LecTimeTable= mongoose.model('LecTimeTable',{
+_id:{
+  type: String,
+  min:5
+},
+tbltype:{
+  type:String,
+  //enum:['lec'],
+  default:'lec'
+},
+row1:{
+   type:[String]
+},
+row2:{
+  type:[String]
+},
+row3:{
+  type:[String]
+},
+row4:{
+  type:[String]
+},
+row6:{
+   type:[String]
+},
+row7:{
+ type:[String]
+},
+row8:{
+   type:[String]
+},
+row9:{
+   type:[String]
+},
+row10:{
+ type:[String]
+},
+row11:{
+   type:[String]
+}
+});
+
+
+//lab model
+var LabTimeTable= mongoose.model('LabTimeTable',{
+_id:{
+  type: String,
+  min:5
+},
+tbltype:{
+  type:String,
+  //enum:['lab'],
+  default:'lab'
+},
+row1:{
+   type:[String]
+},
+row2:{
+  type:[String]
+},
+row3:{
+  type:[String]
+},
+row4:{
+  type:[String]
+},
+row6:{
+   type:[String]
+},
+row7:{
+ type:[String]
+},
+row8:{
+   type:[String]
+},
+row9:{
+   type:[String]
+},
+row10:{
+ type:[String]
+},
+row11:{
+   type:[String]
+}
+});
+
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TimeTable',(err,db)=>{
+  if(err){
+    return console.log('unable to connect to the mongoDB server'); //return out if there is an error
+  }
+  console.log('connected to the mongoDB server');
+
+});
 
 const port=process.env.PORT || 3000;
 const publicPath =path.join(__dirname,'../public');
@@ -14,6 +161,72 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
   console.log('new user connected');
+
+  socket.on('addNewTable',(timeTable)=>{
+
+    if(timeTable.tbltype==="sem"){
+      var newtable= new SemTimeTable({
+       _id     : timeTable.tblid,
+       row1    : timeTable.row1,
+       row2    : timeTable.row2,
+       row3    : timeTable.row3,
+       row4    : timeTable.row4,
+       row6    : timeTable.row6,
+       row7    : timeTable.row7,
+       row8    : timeTable.row8,
+       row9    : timeTable.row9,
+       row10   : timeTable.row10,
+       row11   : timeTable.row11
+
+     });
+     newtable.save();
+    }
+    else if (timeTable.tbltype==="lec") {
+
+        var newtable= new LecTimeTable({
+         _id     : timeTable.tblid,
+         row1    : timeTable.row1,
+         row2    : timeTable.row2,
+         row3    : timeTable.row3,
+         row4    : timeTable.row4,
+         row6    : timeTable.row6,
+         row7    : timeTable.row7,
+         row8    : timeTable.row8,
+         row9    : timeTable.row9,
+         row10   : timeTable.row10,
+         row11   : timeTable.row11
+
+       });
+       newtable.save();
+      }
+
+    else if (timeTable.tbltype==="lab") {
+
+        var newtable= new LabTimeTable({
+         _id     : timeTable.tblid,
+         row1    : timeTable.row1,
+         row2    : timeTable.row2,
+         row3    : timeTable.row3,
+         row4    : timeTable.row4,
+         row6    : timeTable.row6,
+         row7    : timeTable.row7,
+         row8    : timeTable.row8,
+         row9    : timeTable.row9,
+         row10   : timeTable.row10,
+         row11   : timeTable.row11
+
+       });
+       newtable.save();
+      }
+
+
+    //console.log(timeTable.tblid);
+  });
+
+
+
+
+
 
   socket.on('disconnect',()=>{
       console.log('user disconnect');
